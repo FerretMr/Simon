97813d1cf97b72cf034f523ad2de0bd349128a12
feret flowingfert3 <editorferret@gmail.com>
2020-03-16 14:53:24 +0100

    d

--- SimonBot.py
+++ SimonBot.py
@@ -9,7 +9,7 @@ from threading import Thread
 #Common Settings;
 Prefix = '?'
 bot = commands.Bot(command_prefix=Prefix,help_command=None)
-token = 'Njg5MTAxODM4MTIwNjQ4NzI0.Xm-Cpw.kBkPERh4jP7nXMIr2zBD6_dTGtY'
+token = 'Njg5MTAxODM4MTIwNjQ4NzI0.Xm-Epg.fan5sLpFBvA3DHDoVPKY4yJdAAk'
 main_status = "Divine || ?help"
 guild = bot.get_guild(686333272359436359)
 Bad_Words = ['nigga','nigger','niggeran','Nigga','NIGGA','NIggA','niGaa','fuck','f*','f***','f*ck','shit','shitty','fucking','f*cking','fucky','sh*t','shite','ass','asshole','assh*le','bitch','b*tch','dick','d*ck','cunt','c*nt','pussy','p*ssy','c*ck','cock','fucker']
